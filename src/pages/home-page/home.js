3fdb52037384d892fdf572beea22a772aaa6f3ce
Robert Wang <robwang.us@gmail.com>
2023-01-29 12:04:08 -0600

    Fixed shit

--- src/pages/home-page/home.js
+++ src/pages/home-page/home.js
@@ -4,7 +4,12 @@ function HomePage() {
       <div>
         <div className={classes.mission}>Our mission is to empower individuals to make a positive impact on the world through their everyday purchases. By rounding up each transaction to the nearest dollar and donating the difference to charitable causes, we strive to create a ripple effect of generosity and kindness that benefits both our customers and those in need.</div>
         <div className={classes.partitions}>
-          <span className={classes.partition_stats}>this is where we put our stats</span><span className={classes.partition_charities}>this is where we put our charities</span>
+          <div className={classes.partition_stats}>
+            <h1 style={{marginLeft:20, marginTop:15}}> Donations</h1>
+          </div>
+          <div className={classes.partition_charities}>
+            <h2 style ={{marginLeft:20, marginTop:15}} > Charities</h2>
+          </div>
         </div>
       </div>
     );
