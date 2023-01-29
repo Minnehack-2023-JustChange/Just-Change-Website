31fd7bb870003ccc1ff160e2d6b937c9df2ae18e
PeachAuthor <alexiliarski@gmail.com>
2023-01-29 11:53:32 -0600

    account page changes

--- src/pages/account-page/Button.js
+++ src/pages/account-page/Button.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 function Button() {
 
-  function sayHello() {
-    alert('Hello!');
+  function validateInputs() {
+    var x1 = document.getElementById("Charity1").value;
+    var x2 = document.getElementById("Charity2").value;
+    var x3 = document.getElementById("Charity3").value;
+    var x4 = document.getElementById("Charity4").value;
+    var x5 = document.getElementById("Charity5").value;
+    var total = parseInt(x1) + parseInt(x2) + parseInt(x3) + parseInt(x4) + parseInt(x5);
+    if (total != 100) {
+      alert("The total percentage must be 100%! Your total is " + total + "%.");
+    }
+    else{
+        alert("Your preferences have been saved!");
+    }
   }
   
   return (
-    <button onClick={sayHello}>
-      Click me!
+    <button onClick={validateInputs}>
+      Submit
     </button>
   );
 }
--- src/pages/account-page/account.js
+++ src/pages/account-page/account.js
@@ -4,30 +4,42 @@ import Button from "./Button";
 
 function AccountPage() {
     return (<div>
-      <h1>Account</h1>
-        <h2>Choose how your change is divided between your chosen charities!</h2>
+      <h1 className={classes.leftmargin1}>Account</h1>
+        <h2 className={classes.leftmargin2}>Choose how your change is divided between your chosen charities!</h2>
         
-          <div className={classes.inputbox}>
+          <div className={classes.leftmargin2}>
         
-            Charity 1 : <input type="text" id="Charity1" name="name" placeholder = "Percentage value 0 to 100" pattern="[0-9]{0,3}" required maxlength="3"></input> % <span></span>
+            Charity 1 : <input type="number" id="Charity1" name="name" placeholder="0" min="0" max="100" required ></input> % <span></span>
             <p></p>
-            Charity 2 : <input type="text" id="Charity2" name="name" placeholder = "Percentage value 0 to 100" pattern="[0-9]{0,3}" required maxlength="3"></input> % <span></span>
+            Charity 2 : <input type="number" id="Charity2" name="name" placeholder="0" min="0" max="100" required ></input> % <span></span>
             <p></p>
-            Charity 3 : <input type="text" id="Charity3" name="name" placeholder = "Percentage value 0 to 100" pattern="[0-9]{0,3}" required maxlength="3"></input> % <span></span>
+            Charity 3 : <input type="number" id="Charity3" name="name" placeholder="0" min="0" max="100" required ></input> % <span></span>
             <p></p>
-            Charity 4 : <input type="text" id="Charity4" name="name" placeholder = "Percentage value 0 to 100" pattern="[0-9]{0,3}" required maxlength="3"></input> % <span></span>
+            Charity 4 : <input type="number" id="Charity4" name="name" placeholder="0" min="0" max="100" required ></input> % <span></span>
             <p></p>
-            Charity 5 : <input type="text" id="Charity5" name="name" placeholder = "Percentage value 0 to 100" pattern="[0-9]{0,3}" required maxlength="3"></input> % <span></span>
+            Charity 5 : <input type="number" id="Charity5" name="name" placeholder="0" min="0" max="100" required ></input> % <span></span>
             
             <p>Click the "Submit" button to save your preferences.</p>
               <div>
                 <Button>Submit</Button>
               </div>
 
-              
+          
 
           </div>
+          <br></br>
+          <h2 className={classes.leftmargin2}>Sign in to Plaid</h2>
+            <div className={classes.leftmargin2}>
+              <div>
+                <button>Sign In</button>
+              </div>
+              </div>
+          
         </div>
+
+        
+
+
         
     );
 
