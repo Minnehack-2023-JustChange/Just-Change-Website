34821e316016bf74757ebd7b3c3731be6b5213cf
Robert Wang <robwang.us@gmail.com>
2023-01-28 21:26:37 -0600

    updated home page

--- src/pages/home-page/home.js
+++ src/pages/home-page/home.js
@@ -1,6 +1,6 @@
 function HomePage() {
     return (
-      <div>Home Page</div>
+      <div>Our mission is to empower individuals to make a positive impact on the world through their everyday purchases. By rounding up each transaction to the nearest dollar and donating the difference to charitable causes, we strive to create a ripple effect of generosity and kindness that benefits both our customers and those in need.</div>
     );
 }
   
